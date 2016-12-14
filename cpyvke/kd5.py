1417c5daab7e47ab23d13f850b30f8319acf9df3
ipselium <ipselium@gmail.com>
2016-12-14 17:54:49 +0100

    Fix Arg Problem

--- cpyvke/kd5.py
+++ cpyvke/kd5.py
@@ -3,7 +3,7 @@
 #
 # File Name : KernelDaemon5.py
 # Creation Date : Fri Nov  4 21:49:15 2016
-# Last Modified : mer. 14 déc. 2016 12:45:02 CET
+# Last Modified : mer. 14 déc. 2016 17:53:04 CET
 # Created By : Cyril Desjouy
 #
 # Copyright © 2016-2017 Cyril Desjouy <ipselium@free.fr>
@@ -468,8 +468,8 @@ def main(args=None):
 
     args, kernel_id = ParseArgs(lockfile, pidfile)
 
-    sport = Config['comm']['s-port']
-    rport = Config['comm']['r-port']
+    sport = int(Config['comm']['s-port'])
+    rport = int(Config['comm']['r-port'])
     delay = float(Config['daemon']['refresh'])
     verbose = True
 
