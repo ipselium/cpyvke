ee96dabe3c8b235f887d16c72f011682eea1a070
ipselium <ipselium@free.fr>
2018-03-20 11:15:39 +0100

    Bumpversion added to project

--- setup.py
+++ setup.py
@@ -20,7 +20,7 @@
 #
 #
 # Creation Date :
-# Last Modified : mar. 20 mars 2018 10:28:57 CET
+# Last Modified : mar. 20 mars 2018 11:08:03 CET
 """
 -----------
 DOCSTRING
@@ -36,7 +36,7 @@ if sys.version_info < (2,7):
     raise NotImplementedError(
         "Sorry, you need at least Python 2.7 to use cpyvke.")
 
-VERSION = '2.0.1'
+VERSION = '1.1.1'
 
 __doc__ = """\
 A Curses PYthon Variable and Kernel Explorer
